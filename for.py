829c80ed2071f55adf0404649cfefd785569b0d8
Roberto <82552139+robertoribeiroweller@users.noreply.github.com>
2023-10-15 18:51:46 -0300

    Create for.py
    breve explicação do for

---
+++ for.py
@@ -0,0 +1,10 @@
+
+s = 0
+
+for c in range(0,4):
+    n = int(input("digite um valor "))
+    s += n
+
+    # Aqui está a área do código que requer atenção.
+    print(" o somatório de todos os valores foi {}".format(s))
+    # note que ali eu troquei a variavel, pq quem está sendo somado é o s
